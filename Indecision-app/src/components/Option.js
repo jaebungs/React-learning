18974c0c26d3304874c25ec2379a55f6381ab3a1
jaebungs <jaebungs@gmail.com>
2020-11-19 13:16:25 -0500

    update styles

--- Indecision-app/src/components/Option.js
+++ Indecision-app/src/components/Option.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 const Option = (props) => (
     <div>
-        {props.optionText}
-        <button 
-            onClick={(e) => {
-                props.handleDeleteOption(props.optionText)
-            }}
-            >
-        remove
-        </button>
+        <div className="option">
+            <p className="option__text">{props.count}. {props.optionText}</p>
+            <button 
+                className="small-button small-button--link"
+                onClick={(e) => {
+                    props.handleDeleteOption(props.optionText)
+                }}
+                >
+            remove
+            </button>
+        </div>
     </div>
 );
 
