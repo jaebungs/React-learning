b5902ab216934038dea476d4549138916fa4eb54
jaebungs <jaebungs@gmail.com>
2020-11-30 18:12:58 -0500

    Learning redux createStore, dispatch, getState, subscribe & unsubscribe, switch with cases

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-const EditExpensePage = () => <div>This is Edit expense page</div>;
+const EditExpensePage = (props) => {
+    console.log(props)
+    return (
+        <div>
+            This is Edit expense page id of {props.match.params.id}
+        </div>
+    );
+};
 
 export default EditExpensePage;
---
+++ expensify-app/src/playground/redux-101.js
@@ -0,0 +1,61 @@
+ import { createStore } from 'redux';
+
+ const store = createStore((state = { count: 0 }, action) => {
+     switch (action.type){
+         case 'INCREMENT':
+             const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+             return {
+                count: state.count + incrementBy  
+             };
+        case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
+            return {
+                count: state.count - decrementBy
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        case 'SET':
+            return {
+                count: action.count
+            }
+        default:
+            return state;
+     }
+     
+ });
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+    console.log('subscribe activated')
+});
+
+
+//  Action to change state
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 2
+});
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 5
+})
+store.dispatch({
+    type: 'DECREMENT'
+})
+
+store.dispatch({
+    type: 'SET',
+    count: 100
+})
+// unsubscribe()
+
+// store.dispatch({
+//     type: 'RESET'
+// })
+
